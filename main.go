647d0447d7555f115747435d2cd5c338bce26cc1
Raphael Lüthy <raphael@luethy.io>
2023-09-27 21:03:30 +0200

    Add HTTP server functionality to main.go
    The main.go script has been refactored to include an HTTP server function. The function is set to listen and serve on port 8080. The previously implemented functionality for adding, modifying, and iterating over a dynamic array has been commented out to make way for implementing the server. This is in line with shifting our app to a web-based interface, with data management moving from an array to a database or other complex storage system.

--- main.go
+++ main.go
@@ -3,33 +3,37 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 	"os"
 	"raphaelluethy/go-go-br/services/database"
 )
 
 func main() {
 	// Create an empty slice
-	var dynamicArray []int
+	//var dynamicArray []int
 
-	// Add elements to the dynamic array
-	dynamicArray = append(dynamicArray, 10)
-	dynamicArray = append(dynamicArray, 20)
-	dynamicArray = append(dynamicArray, 30)
-
-	// Access and modify elements
-	fmt.Println(dynamicArray[0]) // Output: 10
-
-	dynamicArray[1] = 50
-	fmt.Println(dynamicArray) // Output: [10 50 30]
-
-	// Length and capacity of the dynamic array
-	fmt.Println(len(dynamicArray)) // Output: 3 (number of elements)
-	fmt.Println(cap(dynamicArray)) // Output: 4 (capacity of the underlying array)
-
-	// Iterate over the dynamic array
-	for _, element := range dynamicArray {
-		fmt.Println(element)
-	}
+	//// Add elements to the dynamic array
+	//dynamicArray = append(dynamicArray, 10)
+	//dynamicArray = append(dynamicArray, 20)
+	//dynamicArray = append(dynamicArray, 30)
+	//
+	//// Access and modify elements
+	//fmt.Println(dynamicArray[0]) // Output: 10
+	//
+	//dynamicArray[1] = 50
+	//fmt.Println(dynamicArray) // Output: [10 50 30]
+	//
+	//// Length and capacity of the dynamic array
+	//fmt.Println(len(dynamicArray)) // Output: 3 (number of elements)
+	//fmt.Println(cap(dynamicArray)) // Output: 4 (capacity of the underlying array)
+	//
+	//// Iterate over the dynamic array
+	//for _, element := range dynamicArray {
+	//	fmt.Println(element)
+	//}
+	HTTPServer()
 }
 
 func DoDBThings() {
@@ -56,4 +60,14 @@ func DoDBThings() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func HTTPServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		if err != nil {
+			return
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
